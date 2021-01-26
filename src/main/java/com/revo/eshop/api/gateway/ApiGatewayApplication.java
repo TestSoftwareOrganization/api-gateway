8a81e65a36650a111c0fa9e71c68e1616a1455f2
kolevtest <kolevtestsoftware@gmail.com>
2021-01-26 22:30:02 +0200

    init commit

---
+++ src/main/java/com/revo/eshop/api/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.revo.eshop.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
